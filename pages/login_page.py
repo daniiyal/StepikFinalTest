bbd1df3264aa8d41923da74f4383dfc8173a1c42
daniiyal <danik.magomedow@mail.ru>
2020-06-14 18:49:18 +0300

    LoginPageTest added

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
+import time
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        assert "/login/" in self.browser.current_url, "Некорректный адрес страницы логина"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Нет формы для логина"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Нет формы для регистрации"
